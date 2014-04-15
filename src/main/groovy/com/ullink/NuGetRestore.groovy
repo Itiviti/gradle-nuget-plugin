cb6874e06e4ee331603d2e605902a335013c6cac
Emanuel Varga <emanuel.varga@ullink.com>
2014-04-15 16:23:32 +0300

    Added nugetRestore task to readme
     - fixed tabulation
    
    Change-Id: I3b22f7837397132ba594a1690b16aa4263f50da9

--- src/main/groovy/com/ullink/NuGetRestore.groovy
+++ src/main/groovy/com/ullink/NuGetRestore.groovy
@@ -5,35 +5,35 @@ import org.gradle.api.tasks.StopActionException
 import org.gradle.api.tasks.TaskAction
 
 class NuGetRestore extends BaseNuGet {
-	def projectFolder
-	String restoreFolder
+    def projectFolder
+    def restoreFolder
 
-	NuGetRestore() {
-		super('restore')
-	}
+    NuGetRestore() {
+        super('restore')
+    }
 
-	@Override
-	void verifyCommand() {
-	}
+    @Override
+    void verifyCommand() {
+    }
 
-	@Override
-	List<String> extraCommands() {
-		def commandLineArgs = new ArrayList<String>()
-		if (projectFolder) {
-			def file = new File(projectFolder, "repositories.config");
-			if (!file.exists())
-			{
-				logger.info("NuGet repositories.config not found at path: ${file}, running default package restore.");
-				return commandLineArgs;
-			}
+    @Override
+    List<String> extraCommands() {
+        def commandLineArgs = new ArrayList<String>()
+        if (projectFolder) {
+            def file = new File(projectFolder, "repositories.config");
+            if (!file.exists())
+            {
+                logger.info("NuGet repositories.config not found at path: ${file}, running default package restore.");
+                return commandLineArgs;
+            }
 
-			commandLineArgs += file
-			if (restoreFolder) {
-				commandLineArgs += "-RestoreFolder"
-				commandLineArgs += restoreFolder
-			}
-		}
+            commandLineArgs += file
+            if (restoreFolder) {
+                commandLineArgs += "-RestoreFolder"
+                commandLineArgs += restoreFolder
+            }
+        }
 
-		return commandLineArgs
-	}
+        return commandLineArgs
+    }
 }
