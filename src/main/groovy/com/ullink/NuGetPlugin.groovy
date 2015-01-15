9e5957d95aa782948362fa5bfe6038779650ea0f
Remy Masson <remy.masson@ullink.com>
2015-01-15 13:51:09 +0100

    Make plugin linux-friendly
    - extracted nuget.exe gets +x
    - define the tasks even when not running windows
    
    Change-Id: I2a41bb6f96c31eed9e308a252443a7dcc32ddd84

--- src/main/groovy/com/ullink/NuGetPlugin.groovy
+++ src/main/groovy/com/ullink/NuGetPlugin.groovy
@@ -2,26 +2,22 @@ package com.ullink
 
 import org.gradle.api.Plugin
 import org.gradle.api.Project
-import org.gradle.api.Task;
-import org.gradle.api.plugins.BasePlugin;
-import org.gradle.internal.os.OperatingSystem
+import org.gradle.api.plugins.BasePlugin
 
 class NuGetPlugin implements Plugin<Project> {
     void apply(Project project) {
         project.apply plugin: 'base'
-        if (OperatingSystem.current().windows) {
-            def nuget = project.task('nugetPack', type: NuGetPack)
-			nuget.group = BasePlugin.BUILD_GROUP
-			nuget.description = 'Executes nuget pack command.'
-			
-            nuget = project.task('nugetPush', type: NuGetPush)
-			nuget.group = BasePlugin.UPLOAD_GROUP
-			nuget.description = 'Executes nuget push command.'
-			
-			nuget = project.task('nugetRestore', type: NuGetRestore)
-			nuget.group = BasePlugin.BUILD_GROUP
-			nuget.description = 'Executes nuget package restore command.'
-        }
+        def nuget = project.task('nugetPack', type: NuGetPack)
+        nuget.group = BasePlugin.BUILD_GROUP
+        nuget.description = 'Executes nuget pack command.'
+
+        nuget = project.task('nugetPush', type: NuGetPush)
+        nuget.group = BasePlugin.UPLOAD_GROUP
+        nuget.description = 'Executes nuget push command.'
+
+        nuget = project.task('nugetRestore', type: NuGetRestore)
+        nuget.group = BasePlugin.BUILD_GROUP
+        nuget.description = 'Executes nuget package restore command.'
     }
 }
 
