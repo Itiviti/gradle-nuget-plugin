e03256ebe4dff7de54bc8a34e698285ad4052da0
James Poli <James.Poli@sas.com>
2017-12-04 09:31:06 -0500

    use findByName per Francois; to avoid exception handling overhead

--- src/main/groovy/com/ullink/util/GradleHelper.groovy
+++ src/main/groovy/com/ullink/util/GradleHelper.groovy
@@ -1,14 +1,13 @@
 package com.ullink.util
 
 import org.gradle.api.Project
-import org.gradle.api.UnknownTaskException
 
 class GradleHelper {
     def static String getPropertyFromTask(Project project, String property, String task) {
-        try {
-            def theTask = project.tasks.getByName(task)
+        def theTask =  project.tasks.findByName(task)
+        if (theTask) {
             return theTask.property(property)
-        } catch (UnknownTaskException | MissingPropertyException e ) {
+        } else {
             return null
         }
     }
