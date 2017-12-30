8939c94f3df3c64358f508c285c32964548a7c27
darsen <danijel.arsenovski@gmail.com>
2017-12-30 00:29:00 -0800

    added operations

--- src/main/groovy/com/ullink/NuGetSources.groovy
+++ src/main/groovy/com/ullink/NuGetSources.groovy
@@ -1,10 +1,32 @@
-
 package com.ullink
 
 class NuGetSources extends BaseNuGet {
-  
-   NuGetSources() {
+    enum Operation{
+        add, remove, enable, disable
+    }
+
+    NuGetSources() {
         super('sources')
+    }
+
+    Operation operation
+    def name
+    def source
+    def username
+    def password
+    def configFile
+    bool storePaswordInClearText
+
+    @Override
+    void exec() {
+        args operation
+        if (name) args '-Name', name
+        if (source) args '-Source',source        
+        if (username) args '-UserName', name
+        if (password) args '-Passsword', name
+        if (configFile) args '-ConfigFile', name
+        if (storePaswordInClearText) args '-StorePaswordInClearText'
+        super.exec()
     }
 
 }
