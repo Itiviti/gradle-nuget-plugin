4ec637347693cbb9611e3d96022388e06d52649a
Peter Ng <ngyukman@gmail.com>
2018-09-08 16:30:53 +0800

    Add and fix unit test

--- src/test/groovy/com/ullink/packageparser/PackageReferenceParserSpec.groovy
+++ src/test/groovy/com/ullink/packageparser/PackageReferenceParserSpec.groovy
@@ -12,6 +12,7 @@
 package com.ullink.packageparser
 
 import com.ullink.packagesparser.PackageReferenceParser
+import groovy.xml.MarkupBuilder
 import spock.lang.Specification
 
 class PackageReferenceParserSpec extends Specification {
@@ -23,14 +24,24 @@ class PackageReferenceParserSpec extends Specification {
         def result = new PackageReferenceParser().getDependencies(csproj)
 
         then:
-        result == [
-                [dependency: [id: 'Microsoft.AspNetCore.Mvc.Abstractions', version: '1.1.2']],
-                [dependency: [id: 'Microsoft.AspNetCore.Mvc.Core', version: '1.1.2']],
-                [dependency: [id: 'Microsoft.Extensions.Caching.Abstractions', version: '1.1.1']],
-                [dependency: [id: 'Microsoft.Extensions.Configuration.Binder', version: '1.1.1']],
-                [dependency: [id: 'Newtonsoft.Json', version: '9.0.1']],
-                [dependency: [id: 'StyleCop.Analyzers', version: '1.0.0']],
-                [dependency: [id: 'NuGet.Versioning', version: '3.6.0', include: 'build', exclude: 'none']],
-                [dependency: [id: 'System.Xml.XDocument', version: '4.3.0']]]
+        def writer = new StringWriter()
+        def xml = new MarkupBuilder(writer)
+        xml.dependencies() {
+            result.each {
+                it.resolveStrategy = DELEGATE_FIRST
+                it.delegate = delegate
+                it.call()
+            }
+        }
+        writer.toString() == '''<dependencies>
+  <dependency id='Microsoft.AspNetCore.Mvc.Abstractions' version='1.1.2' />
+  <dependency id='Microsoft.AspNetCore.Mvc.Core' version='1.1.2' />
+  <dependency id='Microsoft.Extensions.Caching.Abstractions' version='1.1.1' />
+  <dependency id='Microsoft.Extensions.Configuration.Binder' version='1.1.1' />
+  <dependency id='Newtonsoft.Json' version='9.0.1' />
+  <dependency id='StyleCop.Analyzers' version='1.0.0' />
+  <dependency id='NuGet.Versioning' version='3.6.0' include='build' exclude='none' />
+  <dependency id='System.Xml.XDocument' version='4.3.0' />
+</dependencies>'''
     }
 }
