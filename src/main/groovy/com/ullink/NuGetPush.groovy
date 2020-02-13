27f8038ea47f796adf552ff6e2bffb00b488c7d2
Ng Yuk Man <ngyukman@gmail.com>
2020-02-13 21:19:56 +0800

    Removing timeout arg in NugetPush
    It's been supported out of the box and conflict in gradle 7

--- src/main/groovy/com/ullink/NuGetPush.groovy
+++ src/main/groovy/com/ullink/NuGetPush.groovy
@@ -3,10 +3,8 @@ package com.ullink
 class NuGetPush extends BaseNuGet {
 
     def nupkgFile
-
     def serverUrl
     def apiKey
-    def timeout
     def configFile
 
     NuGetPush() {
@@ -28,7 +26,6 @@ class NuGetPush extends BaseNuGet {
 
         if (serverUrl) args '-Source', serverUrl
         if (apiKey) args '-ApiKey', apiKey
-        if (timeout) args '-Timeout', timeout
         if (configFile) args '-ConfigFile', configFile
 
         super.exec()
