b46f7ab3e1c2b9bb74580f88a7bd5872e44e4842
Peter Ng <ngyukman@gmail.com>
2020-05-12 09:04:10 +0800

    nuspec cannot be serialized
    Change-Id: I40494aee41ca79de598d2a969164076ec2cf306e

--- src/main/groovy/com/ullink/NuGetSpec.groovy
+++ src/main/groovy/com/ullink/NuGetSpec.groovy
@@ -7,14 +7,14 @@ import com.ullink.packagesparser.ProjectJsonParser
 import groovy.util.slurpersupport.GPathResult
 import groovy.xml.XmlUtil
 import org.gradle.api.tasks.Exec
-import org.gradle.api.tasks.Input
 import org.gradle.api.tasks.Internal
 import org.gradle.api.tasks.OutputFile
 
 class NuGetSpec extends Exec {
 
+    @Internal
     def nuspecFile
-    @Input
+    @Internal
     def nuspec
 
     void exec() {
